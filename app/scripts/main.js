cf1b3f4cdfd4a977fc51159a28c7af3f6c1b434f
Iain Andrew <iainbruceandrew@gmail.com>
2015-02-16 18:58:27 +0000

    now adds .jpg to all imgur links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,22 @@
 $.getJSON('http://www.reddit.com/r/malefashionadvice/search.json?q=selftext:WAYWT = What Are You Wearing Today&syntax=lucene&restrict_sr=true&sort=new&limit=1', function(response) {
   var thread = response.data.children[0].data;
-  $.getJSON(thread.url + '.json?jsonp=?', function(response) {
+  $.getJSON(thread.url + '.json?jsonp=?&sort=top', function(response) {
     var comments = response[1].data.children;
     var images = [];
     for (var i = 0; i < comments.length - 1; i++) {
       var commentLink = comments[i].data.body_html.match(/a href="([^"]*)/)[1]; // comments with a URL present
       if (commentLink.split('.').pop() === 'jpg') { // checks if link ends in .jpg
         images.push(commentLink); 
+      } else if (commentLink.substring(0, 12) == "http://imgur") { // if URL is an imgur link
+        commentLink += '.jpg'; // append .jpg to end of the URL
+        images.push(commentLink); 
       }
     }
+    $('img').error(function(){
+      $(this).hide();
+    });
     for (var i = 0; i < images.length; i ++) {
       $('#images').append('<img src=' + images[i] + ' class="image" />');
-      console.log(images.length);
     }
   });
 });
