5ede1c95beea13d27b86fc1d5ab63598a42c8f4c
Iain Andrew <iainbruceandrew@gmail.com>
2015-02-16 19:11:40 +0000

    don't add imgur albums or galleries to images array

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,20 @@ $.getJSON('http://www.reddit.com/r/malefashionadvice/search.json?q=selftext:WAYW
   $.getJSON(thread.url + '.json?jsonp=?&sort=top', function(response) {
     var comments = response[1].data.children;
     var images = [];
+    var nonImages = [];
     for (var i = 0; i < comments.length - 1; i++) {
       var commentLink = comments[i].data.body_html.match(/a href="([^"]*)/)[1]; // comments with a URL present
       if (commentLink.split('.').pop() === 'jpg') { // checks if link ends in .jpg
         images.push(commentLink); 
-      } else if (commentLink.substring(0, 12) == "http://imgur") { // if URL is an imgur link
+      } else if (commentLink.toLowerCase().indexOf("imgur.com") >= 0 // if URL is an imgur link
+                && commentLink.toLowerCase().indexOf("imgur.com/a/") <= 0 // and it's not an album
+                && commentLink.toLowerCase().indexOf("imgur.com/gallery") <= 0) // or a gallery
+      {
         commentLink += '.jpg'; // append .jpg to end of the URL
         images.push(commentLink); 
       }
     }
+    //console.log(nonImages);
     $('img').error(function(){
       $(this).hide();
     });
