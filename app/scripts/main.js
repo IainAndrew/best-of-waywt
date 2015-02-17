0421eb66be87254862d87d8fb741e2fe282dc1c5
Iain Andrew <iainbruceandrew@gmail.com>
2015-02-17 18:51:17 +0000

    some refactoring, added nicer function to detect url ending

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,18 +4,24 @@ $.getJSON('http://www.reddit.com/r/malefashionadvice/search.json?q=selftext:WAYW
     var comments = response[1].data.children;
     var images = [];
     for (var i = 0; i < comments.length - 1; i++) {
-      var commentLink = comments[i].data.body_html.match(/a href="([^"]*)/)[1]; // comments with a URL present
-      if (commentLink.split('.').pop() === 'jpg') { // checks if link ends in .jpg
-        images.push(commentLink); 
-      } else if (commentLink.toLowerCase().indexOf("imgur.com") >= 0 // if URL is an imgur link
-                && commentLink.toLowerCase().indexOf("imgur.com/a/") <= 0 // and it's not an album
-                && commentLink.toLowerCase().indexOf("imgur.com/gallery") <= 0 // or a gallery
-                && commentLink.toLowerCase().indexOf(",") <= 0) // or has a comma in it
+      var commentLink = comments[i].data.body_html.match(/a href="([^"]*)/)[1]; // extract url from comment
+      function endsWith(str, suffix) {
+          return str.toLowerCase().indexOf(suffix, str.length - suffix.length) !== -1;
+      }
+      if (commentLink.toLowerCase().indexOf("imgur.com") >= 0 // if url is an imgur link
+       && commentLink.toLowerCase().indexOf("imgur.com/a/") <= 0 // and it's not an album
+       && commentLink.toLowerCase().indexOf("imgur.com/gallery") <= 0 // or a gallery
+       && commentLink.toLowerCase().indexOf(",") <= 0 // or has a comma in it
+       && endsWith(commentLink, '.jpg') === false && endsWith(commentLink, '.png') === false)
       {
-        commentLink += '.jpg'; // append .jpg to end of the URL
+        commentLink += '.jpg'; // append .jpg to end of the url
+        images.push(commentLink); 
+      } else if (endsWith(commentLink, '.jpg') === true || endsWith(commentLink, '.png') === true) { // check if link ends in .jpg
         images.push(commentLink); 
       }
+      //console.log(commentLink);
     }
+    console.log(images);
     $('img').error(function(){
       $(this).hide();
     });
