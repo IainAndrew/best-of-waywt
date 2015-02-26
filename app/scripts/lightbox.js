74c57cc344d1dbfcd05df6cc1d400abaf7423623
Iain Andrew <iainbruceandrew@gmail.com>
2015-02-26 23:22:19 +0000

    fixed naming conflict in lightbox.js

--- app/scripts/lightbox.js
+++ app/scripts/lightbox.js
@@ -1,9 +1,8 @@
-var $images = $('#images'),
-    $lightbox = $('#lightbox'),
+var $lightbox = $('#lightbox'),
     $overlay = $('#overlay'),
     $lightboxImage = $('#lightbox-image');
 
-$images.on('click', '.img-link', function(e) {
+$container.on('click', '.img-link', function(e) {
   e.preventDefault();
   $lightbox.addClass('show').find('#lightbox-image').append( '<img src=' + $(this).find('img').attr('src').replace('l.jpg', '.jpg') + '>' );
   $overlay.addClass('show');
